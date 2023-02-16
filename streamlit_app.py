ccca3aeec4ab9a285a13f2f3a1a000081125adf3
AnetTini <125559231+AnetTini@users.noreply.github.com>
2023-02-16 17:34:39 +0530

    Update streamlit_app.py
    created header with three items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner!')
+streamlit.title('My Mom's Healthy Diner!')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
